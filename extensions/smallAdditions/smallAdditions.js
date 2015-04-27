9b10e36e9866fd1d7b84899d0e244859692bfff9
Paul-Hutton <paul@digitalfusion.co.nz>
2015-04-28 10:57:31 +1200

    Notify FileMaker of window load completion. - Includes getUrlParameter helper function.

--- extensions/smallAdditions/smallAdditions.js
+++ extensions/smallAdditions/smallAdditions.js
@@ -72,6 +72,24 @@ $(function() {
   },2000);
 
 
+function getUrlParameter(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+//Notify Filemaker that editor has finished intial loading
+window.onload = function(){
+	window.location = getUrlParameter( "filemakerCommUrl" ) + "Load%20Complete";
+}
+
 });
 
 
